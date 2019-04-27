7309064d56d6affeb6213d5b69f7a2a9803b55f0
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-04-27 22:17:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function myAnimal() {
 }
 
 function yourAnimal(dog) {
-  var dog = cat
+  var cat = dog
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
