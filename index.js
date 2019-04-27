dd2b6cd1dfd2b8c339374c49491d7700d24e39c1
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-04-27 22:26:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal(cat) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  
+
 }
 
 function add2(n) {
